c27ea32a6d2abafd1d4ccdb859e5b204b722b2ad
Jan Pikl <jan.pikl@cninvest.cz>
2019-09-09 09:24:24 +0200

    Start - templates, accessing data, binding, directives, v-once, raw html

--- first/first.js
+++ first/first.js
@@ -1,11 +1,22 @@
 new Vue({
-    el: '#app',
-    data: {
-        title: 'Hello World!'
+  el: '#app',
+  data: {
+    title: 'Hello World!',
+    content: 'This is my funky content.',
+    description: 'Tohle je popis.',
+    link: 'https://pikl.cz',
+    finishedLink: '<a href="http://google.com">Google</a>'
+  },
+  methods: {
+    changeDesc: function(event) {
+      this.description = event.target.value;
     },
-    methods: {
-        changeTitle: function (event) {
-            this.title = event.target.value;
-        }
+    printContent: function() {
+      return this.content;
+    },
+    printNewTitle: function() {
+      this.title = 'H-e-l-l-o-!';
+      return this.title;
     }
+  }
 });
